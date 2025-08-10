f708ed6122b46d90f3ac6400347e3b1c46f6cc4c
cgh0-0 <marketcarrot280@gmail.com>
2025-08-10 12:27:56 +0900

    popback,print,shrink_to_fit
    위의 함수들 추가

--- 가변배열/가변배열/가변배열.cpp
+++ 가변배열/가변배열/가변배열.cpp
@@ -14,7 +14,7 @@ public:
 
 	myvec()
 
-		:m_pdata(nullptr), m_count(0), m_capacity(10)
+		:m_pdata(nullptr), m_count(0), m_capacity(0)
 
 	{
 
@@ -87,4 +87,70 @@ public:
 			}
 		 }
 	}
+	void popback()
+	{
+		m_pdata[m_count-1]=0;
+		--m_count;
+	}
+	void print()
+	{
+		for (int i = 0; i < m_count; ++i)
+		{
+			std::cout << m_pdata[i] << std::endl;
+		}
+	}
+	void shrink_to_fit()
+	{
+		int* newarr = new int[m_count];
+		for (int i = 0; i < m_count; ++i)
+		{
+			
+			newarr[i] = m_pdata[i];
+		}
+		delete[] m_pdata;
+		m_pdata = newarr;
+		m_capacity = m_count;
+	}
 };
+int main()
+{
+	myvec arr;
+	int c = 453;
+	int a = 5345;
+	int b = 324;
+	int d = 12;
+	int v = 2;
+	int o = 21;
+	arr.pushback(&a);
+	arr.pushback(&b);
+	arr.pushback(&c);
+	arr.pushback(&d);
+	arr.pushback(&v);
+	arr.pushback(&o);
+	arr.print();
+	arr.bubblesort();
+	std::cout << "bubble sort" << std::endl;
+	arr.print();
+	a = 1;
+	arr.pushback(&a);
+	std::cout << "push back" << std::endl;
+	b = 999;
+	arr.pushfront(&b);
+	arr.print();
+	std::cout << "push front" << std::endl;
+	arr.print();
+	std::cout << "pop back" << std::endl;
+	arr.popback();
+	arr.print();
+	std::cout << "bubblesort" << std::endl;
+	arr.bubblesort();
+	arr.print();
+	arr.popback();
+	arr.popback();
+	arr.print();
+	arr.shrink_to_fit();
+	std::cout << "after shrink to fit and bubble sort" << std::endl;
+	arr.bubblesort();
+	arr.print();
+	return 0;
+}
